61c0ff0a892abfb50da2518910b03244986ab654
Rexyte <88747126+Rexyte@users.noreply.github.com>
2023-06-30 23:08:05 +0100

    Update kitty conf & fix neovim autocomplete and lsp

--- nvim/.config/nvim/after/plugin/mason.lua
+++ nvim/.config/nvim/after/plugin/mason.lua
@@ -1,6 +1,41 @@
 require("mason").setup()
 require("mason-lspconfig").setup()
 
+-- Setup lspconfig
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 -- After setting up mason-lspconfig you may set up servers via lspconfig
-require("lspconfig").lua_ls.setup {}
-require("lspconfig").rust_analyzer.setup {}
+require("lspconfig").lua_ls.setup {
+    capabilities = capabilities,
+    settings = {
+        Lua = {
+          runtime = {
+            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT',
+          },
+          diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = {'vim'},
+          },
+          workspace = {
+            -- Make the server aware of Neovim runtime files
+            library = vim.api.nvim_get_runtime_file("", true),
+            checkThirdParty = false, -- THIS IS THE IMPORTANT LINE TO ADD
+          },
+          -- Do not send telemetry data containing a randomized but unique identifier
+          telemetry = {
+            enable = false,
+          },
+        },
+    },
+}
+
+require("lspconfig").rust_analyzer.setup {
+    capabilities = capabilities,
+}
+require("lspconfig").clangd.setup {
+    capabilities = capabilities,
+}
+require("lspconfig").pyright.setup {
+    capabilities = capabilities,
+}
